f4bce9d68c70356e63c130015c8dd256a8493fab
Matheus Rogerio <70346606+omateusrogerio@users.noreply.github.com>
2022-09-13 20:18:21 -0300

    Update script.js

--- script.js
+++ script.js
@@ -16,13 +16,13 @@ $(document).ready(function(){
      });
 
      var typed = new Typed(".typing", {
-         strings:["Estudante", "Desenvolvedor", "Freelancer"],
+         strings:["Estudante", "Desenvolvedor", "Estagiário"],
          typeSpeed:100,
          backSpeed:60,
          loop:true
      });
      var typed = new Typed(".typing-2", {
-        strings:["Estudante", "Desenvolvedor", "Freelancer"],
+        strings:["Estudante", "Desenvolvedor", "Estagiário"],
         typeSpeed:100,
         backSpeed:60,
         loop:true
